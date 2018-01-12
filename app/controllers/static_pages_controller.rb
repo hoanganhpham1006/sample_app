53a1794cc817d2468a078e3a754b22e248d23a74
hoanganhpham1006 <hoanganh.pham.12327@gmail.com>
2018-01-12 12:27:12 +0700

    Add users following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,11 @@ class StaticPagesController < ApplicationController
   def home
      if logged_in?
       @micropost  = current_user.microposts.build
-      @feed_items = Micropost.where("user_id = #{current_user.id}").paginate page: params[:page],
-        per_page: Settings.paginate.num_per_page
+      following_ids = "SELECT followed_id FROM relationships
+        WHERE  follower_id = #{current_user.id}"
+      @feed_items = Micropost.where("user_id IN (#{following_ids})
+        OR user_id = #{current_user.id}")
+        .paginate page: params[:page], per_page: Settings.paginate.num_per_page
     end
   end
 
